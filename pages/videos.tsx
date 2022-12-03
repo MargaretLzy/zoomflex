f72043f5441ef19343036766f2aaed5ad55a5c82
MargaretLzy <43190680+MargaretLzy@users.noreply.github.com>
2022-12-03 13:29:54 -0500

    Update videos.tsx

--- pages/videos.tsx
+++ pages/videos.tsx
@@ -1,7 +1,32 @@
-import type { NextPage } from "next"
+import type { NextPage } from "next";
+import React, {useEffect, useState} from 'react';
 
-const Videos: NextPage = () => {
-  return <div>Videos Page</div>
+function Videos(){
+const[videos,setVideos]= useState([]);
+useEffect(()=>{
+  const getVideos = async()=>{
+    const res = await fetch('http://localhost/video/videos');
+    const getdata = await res.json();
+    setVideos(getdata);
+    console.log(getdata);
+  }
+  getVideos();
+
+},[]);
+  return( 
+    <React.Fragment>
+<div>Videos Page</div>
+ {videos.map((v)=>(
+<tr key = {v.id}>{v.name}
+<img src={v.images}/>
+</tr>
+
+ )
+ )
+}
+  
+    </React.Fragment> 
+  );
 }
 
 export default Videos
